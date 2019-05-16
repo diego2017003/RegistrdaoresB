143420ea1c9ebde4141675eac2629428f15a8c1e
diego2017003 <30417399+diego2017003@users.noreply.github.com>
2019-05-16 16:09:37 -0300

    Add files via upload

--- ffd4b.vhd
+++ ffd4b.vhd
@@ -2,16 +2,16 @@ LIBRARY ieee;
 use ieee.std_logic_1164.all;
 ENTITY ffd4b is 
 	port(Dff4: IN STD_LOGIC_VECTOR(3 downto 0);
-		clkff4: IN STD_LOGIC;
+		clkff4,CLRN4: IN STD_LOGIC;
 		Qff4:OUT STD_LOGIC_VECTOR(3 downto 0));
 END ffd4b;
 ARCHITECTURE behaviorffd4b of ffd4b is
 		COMPONENT ffd
-			port(Dff,clkff:IN STD_LOGIC;
+			port(Dff,clkff,CLRN:IN STD_LOGIC;
 		   Qff:OUT STD_LOGIC);
 		END COMPONENT;
 		begin	
 			gen:FOR i IN 0 TO 3 GENERATE
-			l:ffd PORT MAP(Dff=>Dff4(i),clkff=>clkff4,Qff=>Qff4(i));
+			l:ffd PORT MAP(Dff=>Dff4(i),clkff=>clkff4,CLRN=>CLRN4,Qff=>Qff4(i));
 	    END GENERATE;
-END behaviorffd4b;
+END behaviorffd4b;	
--- registradores.vhd
+++ registradores.vhd
@@ -1,26 +1,40 @@
 LIBRARY ieee;
 use ieee.std_logic_1164.all;
-ENTITY REGISTRADORES IS 
-	PORT(E:IN STD_LOGIC_vector(3 downto 0);
-	Sr0,Sr1,Sr2,Sr3,ld,clkr:IN STD_logic);
-END REGISTRADORES;
-ARCHITECTURE registrador OF REGISTRADORES IS
-COMPONENT ffd4b
-			port(Dff4: IN STD_LOGIC_VECTOR(3 downto 0);
-			clkff4:IN STD_LOGIC;
-		   Qff4:OUT STD_LOGIC_VECTOR(3 downto 0));
-		END COMPONENT;
-SIGNAL R0,R1,R2,R3,R4,R5,R6,R7:STD_LOGIC_vector(3 DOWNTO 0);
-SIGNAL Ei:STD_LOGIC_vector(3 DOWNTO 0);
+ENTITY registradoresv2 IS
+	PORT(rw,clk,load,edr2,edr1,edr0:IN STD_LOGIC;
+		fil:IN STD_LOGIC_VECTOR(3 Downto 0);
+		Sout:OUT STD_LOGIC_VECTOR(3 Downto 0));
+END registradoresv2;
+Architecture registra of registradoresv2 is
+SIGNAL Ri,E0,S0,r0,r1,r2,r3,r4,r5,r6,r7:STD_LOGIC_VECTOR(3 Downto 0);
+SIGNAL l0,l1,l2,l3,l4,l5,l6,l7:STD_LOGIC;
+COMPONENT ffd4b IS
+	port(Dff4: IN STD_LOGIC_VECTOR(3 downto 0);
+		clkff4,CLRN4: IN STD_LOGIC;
+		Qff4:OUT STD_LOGIC_VECTOR(3 downto 0));
+	END COMPONENT;
 BEGIN
-Ei<="0000";
-u0: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R0);
-u1: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R1);
-u2: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R2);
-u3: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R3);
-u4: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R4);
-u5: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R5);
-u6: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R6);
-u7: ffd4b PORT MAP(Dff4=>Ei,clkff4=>clkr,Qff4=>R7);
-
-END registrador;
+l0<=(NOT(edr2))AND(NOT(edr1))AND(NOT(edr0))AND(load); 
+l1<=(NOT(edr2))AND(NOT(edr1))AND((edr0))AND(load);
+l2<=(NOT(edr2))AND((edr1))AND(NOT(edr0))AND(load);
+l3<=(NOT(edr2))AND((edr1))AND((edr0))AND(load);
+l4<=(edr2)AND(NOT(edr1))AND(NOT(edr0))AND(load);
+l5<=((edr2))AND(NOT(edr1))AND((edr0))AND(load);
+l6<=((edr2))AND((edr1))AND(NOT(edr0))AND(load);
+l7<=((edr2))AND((edr1))AND((edr0))AND(load);
+	T0:ffd4b port map(Dff4=>fil,clkff4=>(clk AND load),CLRN4=>'0',Qff4=>E0);
+	g0:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l0),CLRN4=>'0',Qff4=>r0);
+	g1:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l1),CLRN4=>'0',Qff4=>r1);
+	g2:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l2),CLRN4=>'0',Qff4=>r2);
+	g3:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l3),CLRN4=>'0',Qff4=>r3);
+	g4:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l4),CLRN4=>'0',Qff4=>r4);
+	g5:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l5),CLRN4=>'0',Qff4=>r5);
+	g6:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l6),CLRN4=>'0',Qff4=>r6);
+	g7:ffd4b port map(Dff4=>fil,clkff4=>((rw)AND clk AND l7),CLRN4=>'0',Qff4=>r7);
+Ri(0)<=NOT(rw) and((l0 AND r0(0))OR(l1 AND r1(0))OR(l2 AND r2(0))OR(l3 AND r3(0))OR(l4 AND r4(0))OR(l5 AND r5(0))OR(l6 AND r6(0))OR(l7 AND r7(0)));
+Ri(1)<=NOT(rw) and((l0 AND r0(1))OR(l1 AND r1(1))OR(l2 AND r2(1))OR(l3 AND r3(1))OR(l4 AND r4(1))OR(l5 AND r5(1))OR(l6 AND r6(1))OR(l7 AND r7(1)));
+Ri(2)<=NOT(rw) and((l0 AND r0(2))OR(l1 AND r1(2))OR(l2 AND r2(2))OR(l3 AND r3(2))OR(l4 AND r4(2))OR(l5 AND r5(2))OR(l6 AND r6(2))OR(l7 AND r7(2)));
+Ri(3)<=NOT(rw) and((l0 AND r0(3))OR(l1 AND r1(3))OR(l2 AND r2(3))OR(l3 AND r3(3))OR(l4 AND r4(3))OR(l5 AND r5(3))OR(l6 AND r6(3))OR(l7 AND r7(3)));
+	Sa0:ffd4b port map(Dff4=>Ri,clkff4=>(NOT(rw)AND clk),CLRN4=>'0',Qff4=>S0);
+	Sout<=S0;
+END registra; 
